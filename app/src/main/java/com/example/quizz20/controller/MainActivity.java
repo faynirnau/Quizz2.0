bd3327064e7ca9c223475ae65bbaab8cbfde93cf
naude <naudet.jeremy@laposte.net>
2023-03-06 17:57:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/quizz20/controller/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.quizz20.controller;
+
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.quizz20.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Access a Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    }
+}
