43bde899c6f912336eb2b90151ee35e902491617
faynirnau <naudet.jeremy@laposte.net>
2023-03-06 18:38:09 +0100

    Inital Commit

--- app/src/main/java/com/example/quizz20/controller/question4Fragment.java
+++ app/src/main/java/com/example/quizz20/controller/question4Fragment.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.example.quizz20.R;
 
-public class Question5Fragment extends Fragment {
+public class question4Fragment extends Fragment {
     //Constructeur
-    public Question5Fragment() {
+    public question4Fragment() {
 
     }
 
@@ -20,6 +20,6 @@ public class Question5Fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_question5, container, false);
+        return inflater.inflate(R.layout.fragment_question4, container, false);
     }
 }
