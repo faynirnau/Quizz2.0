ecf32ab159a15023815cbd1b95d3f0811bf1322e
faynirnau <naudet.jeremy@laposte.net>
2023-03-09 18:16:45 +0100

    Avancé de l'app, design de la page d'accueil plus création de l'interface de jeux

--- app/src/main/java/com/example/quizz20/controller/QuestionActivity.java
+++ app/src/main/java/com/example/quizz20/controller/QuestionActivity.java
@@ -1,15 +1,23 @@
 package com.example.quizz20.controller;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.quizz20.R;
 
 public class QuestionActivity extends AppCompatActivity {
 
-
+    Button boutonA;
+    Button boutonB;
+    Button boutonC;
+    Button boutonD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+        boutonA = findViewById(R.id.reponseA);
+        boutonB = findViewById(R.id.reponseB);
+        boutonC = findViewById(R.id.reponseC);
+        boutonD = findViewById(R.id.reponseD);
     }
 }
