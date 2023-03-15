b3a886c588ba25ebcb1dd80dca41bacac5159a05
faynirnau <naudet.jeremy@laposte.net>
2023-03-15 19:28:05 +0100

    Un objet "Question" a été creer il faut finir de lier la BDD.

--- app/src/main/java/com/example/quizz20/controller/QuestionFragment.java
+++ app/src/main/java/com/example/quizz20/controller/QuestionFragment.java
@@ -1,6 +1,8 @@
 package com.example.quizz20.controller;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,11 @@ import com.example.quizz20.R;
 
 public class QuestionFragment extends Fragment {
     View view;
+    TextView textViewQuestion;
+    Button buttonA;
+    Button buttonB;
+    Button buttonC;
+    Button buttonD;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
